7e90977d68e73d962ddd46fee934b047bab92f99
Abdalla Elbahrawy <71684437+bahrawyyy@users.noreply.github.com>
2023-07-08 19:32:15 +0300

    Atmega32 Ver.2 Uploaded
    Large numbers are supported :)

---
+++ Atmega32_Calculator/Debug/APP/main.d
@@ -0,0 +1,27 @@
+APP/main.o APP/main.o: ../APP/main.c ../APP/../HAL/Lcd/Lcd.h \
+  ../APP/../HAL/Lcd/Lcd_Config.h \
+  ../APP/../HAL/Lcd/../../MCAL/Dio/Dio_interface.h \
+  ../APP/../HAL/Lcd/../../MCAL/Dio/../../LIB/STD_Types.h \
+  ../APP/../HAL/Lcd/../../MCAL/Dio/../../LIB/ErrorState.h \
+  ../APP/../HAL/Lcd/../../MCAL/Dio/../../LIB/Bit_Math.h \
+  ../APP/../HAL/KPD/KPD_interface.h \
+  ../APP/../HAL/KPD/../../LIB/ErrorState.h \
+  ../APP/../HAL/KPD/../../LIB/STD_Types.h
+
+../APP/../HAL/Lcd/Lcd.h:
+
+../APP/../HAL/Lcd/Lcd_Config.h:
+
+../APP/../HAL/Lcd/../../MCAL/Dio/Dio_interface.h:
+
+../APP/../HAL/Lcd/../../MCAL/Dio/../../LIB/STD_Types.h:
+
+../APP/../HAL/Lcd/../../MCAL/Dio/../../LIB/ErrorState.h:
+
+../APP/../HAL/Lcd/../../MCAL/Dio/../../LIB/Bit_Math.h:
+
+../APP/../HAL/KPD/KPD_interface.h:
+
+../APP/../HAL/KPD/../../LIB/ErrorState.h:
+
+../APP/../HAL/KPD/../../LIB/STD_Types.h:
